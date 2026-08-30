aed77394b6e06889fea560d18cfb53c5ff5efcad
Joshua Díaz <joshuagabrieldm@gmail.com>
2026-08-30 15:34:38 -0600

    Update logo text in adminlte configuration

--- config/adminlte.php
+++ config/adminlte.php
@@ -64,7 +64,7 @@ return [
     |
     */
 
-    'logo' => '<b>POS: </b>JILS',
+    'logo' => '<b>PDS: </b>JILS',
     'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
